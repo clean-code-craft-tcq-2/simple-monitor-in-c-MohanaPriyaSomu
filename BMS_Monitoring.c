8b9321039fe21d1b34ab916702746989a64cfd2b
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2022-02-10 12:39:16 +0530

    Update BMS_Monitoring.c

--- BMS_Monitoring.c
+++ BMS_Monitoring.c
@@ -38,7 +38,7 @@ struct BattManagementSystem
     float batteryChargeRate;
 };
 
-void validityCheck (int validity, const char* param)
+void validityCheck (int validity, char* param)
 {
 	char consoleOutput[100];
 	if (validity == 0)
