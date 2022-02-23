a1d91b1ab7419f8bbe1eecaaeb92ac1484b21874
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2022-02-23 22:19:06 +0530

    Update BMS_Monitoring.c

--- BMS_Monitoring.c
+++ BMS_Monitoring.c
@@ -53,10 +53,10 @@ int BatteryStateOk(float temp, float SoC, float battChargeRate, char tempUnit)
 
 void main()
 {
-    //struct BattManagementSystem bms = {30,60,0.5,'K'};
-    //assert(BatteryStateOk(bms.Temperature, bms.stateOfCharge, bms.batteryChargeRate, bms.tempFormat) == 1);
+    struct BattManagementSystem bms = {30,60,0.5,'C'};
+    assert(BatteryStateOk(bms.Temperature, bms.stateOfCharge, bms.batteryChargeRate, bms.tempFormat) == 1);
     struct BattManagementSystem bms1 = {50,90,0.9,'F'};
-    assert(BatteryStateOk(bms1.Temperature, bms1.stateOfCharge, bms1.batteryChargeRate, bms1.tempFormat) == 1);	
+    assert(BatteryStateOk(bms1.Temperature, bms1.stateOfCharge, bms1.batteryChargeRate, bms1.tempFormat) == 0);	
     struct BattManagementSystem bms2 = {60,10,1.4,'K'};
-    assert(BatteryStateOk(bms2.Temperature, bms2.stateOfCharge, bms2.batteryChargeRate, bms2.tempFormat) == 1);
+    assert(BatteryStateOk(bms2.Temperature, bms2.stateOfCharge, bms2.batteryChargeRate, bms2.tempFormat) == 0);
 } 
